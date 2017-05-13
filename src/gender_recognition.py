fe9569f74c0b86efae36e2f7d97050288383eecf
Martin <martinappo@gmail.com>
2017-05-13 06:28:28 -0700

    Gender recognition - move to class

--- src/gender_recognition.py
+++ src/gender_recognition.py
@@ -1,107 +1,111 @@
-import cv2
-import time
 import os
 from PIL import Image
 import numpy as np
 from sklearn.decomposition import FastICA
 from sklearn import svm
 from image_preparation import crop_face
-import matplotlib.pyplot as plt
 
-def evaluateAndCountGenders(ica, classifier, inputFolder="../result_faces"):
-    resultFaces = []
-    femaleCount = 0
-    maleCount = 0
+class GenderRegocnition:
 
-    print("Target;Female count;Male count")
+    predictionMethod = "ica"
 
-    try:
-        themeDirs = os.listdir(inputFolder)
+    def evaluateAndCountGenders(self, ica=None, classifier=None, inputFolder="../result_faces"):
+        femaleCount = 0
+        maleCount = 0
 
-        # Go through all theme directiories
-        for themeDirName in themeDirs:
-            topicFemaleCount = 0
-            topicMaleCount = 0
-            videoNameDirPath = "%s/%s" % (inputFolder, themeDirName)
-            videoDirNames = os.listdir(videoNameDirPath)
+        print("Target;Female count;Male count")
 
-            # Go trough all video directories
-            for videoDirName in videoDirNames:
-                vidFemaleCount = 0
-                vidMaleCount = 0
-                faceImagesDirPath = "%s/%s" % (videoNameDirPath, videoDirName)
-                faceImages = os.listdir(faceImagesDirPath)
+        try:
+            themeDirs = os.listdir(inputFolder)
 
-                # Go through all face images
-                for faceImageName in faceImages:
-                    faceImagePath = "%s/%s" % (faceImagesDirPath, faceImageName)
-                    faceImg = crop_face(Image.open(faceImagePath), offset_pct=(0.1,0.1))
-                    flattenFaceImg = np.array(faceImg).flatten()
+            # Go through all theme directiories
+            for themeDirName in themeDirs:
+                topicFemaleCount = 0
+                topicMaleCount = 0
+                videoNameDirPath = "%s/%s" % (inputFolder, themeDirName)
+                videoDirNames = os.listdir(videoNameDirPath)
 
-                    features = ica.transform([flattenFaceImg])
-                    result = classifier.predict(features)
-                    if (result[0] == "f"):
-                        vidFemaleCount +=1
-                        topicFemaleCount +=1
-                        femaleCount +=1
-                    else:
-                        vidMaleCount +=1
-                        topicMaleCount += 1
-                        maleCount +=1
+                # Go trough all video directories
+                for videoDirName in videoDirNames:
+                    vidFemaleCount = 0
+                    vidMaleCount = 0
+                    faceImagesDirPath = "%s/%s" % (videoNameDirPath, videoDirName)
+                    faceImages = os.listdir(faceImagesDirPath)
 
-                print("Video " + videoDirName + ";" + str(vidFemaleCount) + ";" + str(vidMaleCount))
+                    # Go through all face images
+                    for faceImageName in faceImages:
+                        faceImagePath = "%s/%s" % (faceImagesDirPath, faceImageName)
+                        faceImg = crop_face(Image.open(faceImagePath), offset_pct=(0.1,0.1))
+                        flattenFaceImg = np.array(faceImg).flatten()
 
-            print("Topic " + themeDirName + ";" + str(topicFemaleCount) + ";" + str(topicMaleCount))
+                        if (self.predictionMethod == "ica"):
+                            features = ica.transform([flattenFaceImg])
+                            result = classifier.predict(features)
 
-        print("Total;" + str(femaleCount) + ";" + str(maleCount))
+                        if (result[0] == "f"):
+                            vidFemaleCount +=1
+                            topicFemaleCount +=1
+                            femaleCount +=1
+                        elif (result[0] == "m"):
+                            vidMaleCount +=1
+                            topicMaleCount += 1
+                            maleCount +=1
 
-    except os.error as e:
-        print(e)
+                    print("Video " + videoDirName + ";" + str(vidFemaleCount) + ";" + str(vidMaleCount))
+
+                print("Topic " + themeDirName + ";" + str(topicFemaleCount) + ";" + str(topicMaleCount))
+
+            print("Total;" + str(femaleCount) + ";" + str(maleCount))
+
+        except os.error as e:
+            print(e)
 
 
+    def loadTrainingImages(self, prefix, imgs, targets):
+        imgDirPath = '../train_faces/' + prefix
 
-def loadTrainingImages(prefix, imgs, targets):
-    imgDirPath = '../train_faces/' + prefix
+        try:
+            imgPaths = os.listdir(imgDirPath)
 
-    try:
-        imgPaths = os.listdir(imgDirPath)
+            for imgPath in imgPaths:
+                imgFullPath = "%s/%s" % (imgDirPath, imgPath)
+                img = crop_face(Image.open(imgFullPath).convert('RGB'), offset_pct=(0.3,0.3))
+                imgs.append(np.array(img).flatten())
+                targets.append(prefix)
 
-        for imgPath in imgPaths:
-            imgFullPath = "%s/%s" % (imgDirPath, imgPath)
-            img = crop_face(Image.open(imgFullPath).convert('RGB'), offset_pct=(0.3,0.3))
-            imgs.append(np.array(img).flatten())
-            targets.append(prefix)
+        except os.error as e:
+            print(e)
 
-    except os.error as e:
-        print(e)
+    def getIcaAndClassifier(self, imgs, targets, componentsCnt):
+        # Calc ICA
+        ica = FastICA(n_components=componentsCnt, whiten=True)
+        ica.fit(imgs)
+        features = ica.transform(imgs)
 
-def getIcaAndClassifier(imgs, targets, componentsCnt):
-    # Calc ICA
-    ica = FastICA(n_components=componentsCnt, whiten=True)
-    ica.fit(imgs)
-    features = ica.transform(imgs)
+        # Fit SVM
+        classifier = svm.SVC(gamma=0.01, C=componentsCnt)
+        classifier.fit(features, targets)
 
-    # Fit SVM
-    classifier = svm.SVC(gamma=0.001, C=componentsCnt)
-    classifier.fit(features, targets)
+        # Predict
+        print("Prediction done with " + str(componentsCnt) + " features")
 
-    # Predict
-    print("Prediction done with " + str(componentsCnt) + " features")
+        return ica, classifier
 
-    return ica, classifier
+    def evaluateGenders(self):
+        if (self.predictionMethod == "ica"):
+            trainingImgs = []
+            targets = []
 
-def evaluateGenders():
-    trainingImgs = []
-    targets = []
+            self.loadTrainingImages('m', trainingImgs, targets)
+            self.loadTrainingImages('f', trainingImgs, targets)
 
-    loadTrainingImages('m', trainingImgs, targets)
-    loadTrainingImages('f', trainingImgs, targets)
+            ica, classifier = self.getIcaAndClassifier(trainingImgs, targets, 25)
 
-    ica, classifier = getIcaAndClassifier(trainingImgs, targets, 10)
-
-    evaluateAndCountGenders(ica, classifier)
-
+            self.evaluateAndCountGenders(ica, classifier)
+        elif (self.predictionMethod == "angus"):
+            self.evaluateAndCountGenders()
 
 
 if __name__ == "__main__":
-    evaluateGenders()
+    genderRecognition = GenderRegocnition()
+    genderRecognition.evaluateGenders()
